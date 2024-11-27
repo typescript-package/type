2485c314a6d8c24665ce050b3e122d7f293a6d8e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-27 00:23:56 +0000

    Initial commit.

---
+++ src/public-api.ts
@@ -0,0 +1,21 @@
+/*
+ * Public API Surface of type
+ */
+export type { AnyBoolean } from './lib/any-boolean.type';
+export type { AnyNumber } from './lib/any-number.type';
+export type { AnyString } from './lib/any-string.type';
+export type { CallbackPayload } from './lib/callback-payload.type';
+export type { Constructor } from './lib/constructor.type';
+export type { Defined } from './lib/defined.type';
+export type { ForEachCallback } from './lib/foreach-callback.type';
+export type { GenericObject } from './lib/generic-object.type';
+export type { Never } from './lib/never.type';
+export type { NotUndefined } from './lib/not-undefined.type';
+export type { NumberBetween } from './lib/number-between.type';
+export type { Primitive } from './lib/primitive.type';
+export type { Primitives } from './lib/primitives.type';
+export type { ResultCallback } from './lib/result-callback.type';
+export type { StringOfLength } from './lib/string-of-length.type';
+export type { Type } from './lib/type.type';
+export type { Types } from './lib/types.type';
+export type { Undefined } from './lib/undefined.type';
